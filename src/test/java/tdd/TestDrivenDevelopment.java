3b395df01dc76dfea06c9e68a3dec25a31a96f12
kobeomseok95 <37062337+kobeomseok95@users.noreply.github.com>
2020-11-06 22:37:30 +0900

    Feat : testMultiplication() 작성 및 리팩토링 (1-1장 완료)

--- src/test/java/tdd/TestDrivenDevelopment.java
+++ src/test/java/tdd/TestDrivenDevelopment.java
@@ -2,10 +2,27 @@ package tdd;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 public class TestDrivenDevelopment {
+
+    class Dollar{
+        int amount;
+
+        public Dollar(int amount){
+            this.amount = amount;
+        }
+
+        void times(int multiplier){
+            amount *= multiplier;
+        }
+    }
+
 
     @Test
     public void testMultiplication(){
-
+        Dollar five = new Dollar(5);
+        five.times(2);
+        assertEquals(10, five.amount);
     }
 }
