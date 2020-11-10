8763ee07fa7be8af0f908d8effd5ca8bdac3c3fb
kobeomseok95 <37062337+kobeomseok95@users.noreply.github.com>
2020-11-10 21:48:55 +0900

    Feat : 환율을 계산하기 위한 HashMap 생성(14장까지 마무리)

--- src/test/java/tdd/chapterOne/Bank.java
+++ src/test/java/tdd/chapterOne/Bank.java
@@ -1,8 +1,32 @@
 package tdd.chapterOne;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Bank {
 
+    private Map<Pair, Integer> rates = new HashMap<>();
+
+    public void addRate(String from, String to, int rate){
+        rates.put(new Pair(from, to), new Integer(rate));
+    }
+
     public Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        return source.reduce(this, to);
+    }
+
+    public int rate(String from, String to){
+        if (from.equals(to)) return 1;
+
+        Integer rate = (Integer) rates.get(new Pair(from, to));
+        return rate.intValue();
     }
 }
+
+
+
+
+
+
+
+
